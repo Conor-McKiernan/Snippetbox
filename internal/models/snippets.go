0ff1ca52f1fb6e5951c74d8eeadf7fd59a5a1a5b
Conor McKiernan <c.mckiernan99@gmail.com>
2022-10-15 20:13:11 +0100

    Create Get() on SnippetModel and create errors.go model

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -35,7 +36,26 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	// QueryRow() method on connection pool to execute statement
+	row := m.DB.QueryRow(stmt, id)
+
+	// Copy values from row to the corresponding fields in the Snippet struct
+	s := &Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		// Check if Scan() returned ErrNoRows error
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	// Everything ok, return Snippet object
+	return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
