b9b25279e114c1cc38cab8a6dd9b9ab3ea207004
Conor McKiernan <c.mckiernan99@gmail.com>
2022-10-20 13:09:17 +0100

    Parse files once and store parsed templates in an in-mem cache

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,51 @@
 package main
 
-import "snippetbox.conormckiernan.net/internal/models"
+import (
+	"path/filepath"
+	"text/template"
+
+	"snippetbox.conormckiernan.net/internal/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	// Get slice of all filepaths that match "./ui/html/pages/*.tmpl" to have
+	// application 'page' templates like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		// Parse the base template file into a template set
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseGlob() *on this template set* to add any partials
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() *on this template set* to add the page template
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add template set to map, using name of page as key (like 'home.tmpl')
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
